cebbfb281eda068e32fbb0ca085222b3b1d7d9aa
Natassya <naty.ays@gmail.com>
2020-01-13 11:37:57 -0300

    Atualização da criptografia e decriptografia usando o AES para a Raspberry Pi.

--- Aplicação/Micros/Raspberry Pi/teste.c
+++ Aplicação/Micros/Raspberry Pi/teste.c
@@ -1,35 +1,38 @@
 /*
  * teste.c
- * 
+ *
  * Copyright 2020  <pi@raspberrypi>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
 
 #include <stdio.h>
 #include <string.h>
-#include </home/pi/Documents/relic/include/relic_bc.h> //  local onde está o include da lib
+#include <relic_bc.h> //  local onde está o include da lib
+//#include </home/pi/Documents/relic/include/relic_bc.h> //  local onde está o include da lib
+
 #define BC_len 16
 
 int main()
 {
-	int outputsize,out_len;
+	int outputsize = 32;
+	int out_len = 32;
 	/* A 128 bit key */
 	uint8_t  *key = "0123456789012345";
 
@@ -37,36 +40,31 @@ int main()
     uint8_t *iv = "0123456789012345";
 
      /* Buffer for the decrypted text */
-    uint8_t decryptedtext[BC_len];
-    uint8_t ciphertext[BC_len];
+    uint8_t decryptedtext[32];
+    uint8_t ciphertext[32];
     uint8_t* mensagem= "012345678901234";
-   
-  
+
   // criptografando mensagem
-	
-	if(bc_aes_cbc_enc(ciphertext,&outputsize,mensagem,BC_len,key,BC_len*2,iv)){
-		
+
+	if(bc_aes_cbc_enc(ciphertext,&outputsize,mensagem,16,key,BC_len,iv)){
 		printf("ERRO\n");
-		
+
 	}else{
 		printf("Criptografado com sucesso\n");
 	}
-	
-	// decriptografando texto	
-	if(bc_aes_cbc_dec(decryptedtext,&out_len,ciphertext,BC_len,key,BC_len,iv)){
-		
+
+	printf("Outputsize %d\n", outputsize);
+
+	// decriptografando texto
+	if(bc_aes_cbc_dec(decryptedtext,&out_len,ciphertext,outputsize,key,BC_len,iv)){
+
 		printf("ERRO\n");
-		
+
 	}else{
 		printf("Descriptografado com sucesso\n");
 	}
-	
-	//printf("%s",decryptedtext);
-	
-		
-	
-		
-	
+
+	printf("Decryptedtext: %s\n",decryptedtext);
+
 	return 0;
 }
-
