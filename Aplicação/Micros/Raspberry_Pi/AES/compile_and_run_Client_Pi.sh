cd3e6b05d34bb34f4080327adec992e1e2465076
Augusto <augusto.c.ferreira@hotmail.com>
2020-03-30 14:04:24 -0300

    teste

--- Aplicação/Micros/Raspberry_Pi/AES/compile_and_run_Client_Pi.sh
+++ Aplicação/Micros/Raspberry_Pi/AES/compile_and_run_Client_Pi.sh
@@ -6,15 +6,22 @@ gcc crypto_rasp.c Client.c \
     -o client
 
 
-: '	for i in {0..20}
+	for i in {0..40}
 do
-	./client $i "criptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidor" 10.0.0.9 
+	./client $i "criptografando mensagens para realizar testes" 127.0.0.1 
 	sleep 3s
-done'
+done
 
-	for i in {0..20}
+gcc crypto_rasp.c Client.c \
+    -I/home/pi/relic/relic-target/include \
+    -I/home/pi/relic/include \
+    -L/home/pi/relic/relic-target/lib \
+    -lrelic_s \
+    -o client
+
+	for i in {0..40}
 do
-	./client $i -f /home/pi/Documents/criptografia/Aplicação/Micros/Raspberry_Pi/AES/Imagens/pao_na_chapa.jpg 10.0.0.9 
+	./client $i -f /home/pi/Documents/criptografia/Aplicação/Micros/Raspberry_Pi/AES/Imagens/cripto.jpg /home/pi/Documents/criptografia/Aplicação/Micros/Raspberry_Pi/AES/Imagens/Confirmação_Client/img$i.jpg 127.0.0.1 
 	sleep 3s
 done
 
