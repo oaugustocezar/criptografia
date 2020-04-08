358929b5b86dc64fd13efcb455b869d44c525513
Augusto Cézar <augusto.c.ferreira@hotmail.com>
2020-04-08 13:53:12 -0300

    adicionando .gitignore

--- Aplicação/Micros/Raspberry_Pi/AES/compile_and_run_Client.sh
+++ Aplicação/Micros/Raspberry_Pi/AES/compile_and_run_Client.sh
@@ -3,18 +3,18 @@ gcc crypto_rasp.c Client.c \
     -I/home/augusto/relic/include \
     -L/home/augusto/relic/relic-target/lib \
     -lrelic_s \
-    -o client 
+    -o client.run 
 
 
 	for i in {0..50}
 do
-	./client $i "criptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidor" 127.0.0.1
+	./client.run $i "criptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidorcriptografando mensagens para realizar testes de tempo de criptografia e decriptografia em um sistema de comunicação cliente-servidor" 127.0.0.1
 	sleep 3s
 done
 
 	for i in {0..50}
 do
-	./client $i -f /home/augusto/Documentos/criptografia/Aplicação/Micros/Raspberry_Pi/AES/Imagens/foto.jpg 127.0.0.1
+	./client.run $i -f /home/augusto/Documentos/criptografia/Aplicação/Micros/Raspberry_Pi/AES/Imagens/foto.jpg 127.0.0.1
 	sleep 3s
 done
 
--- Aplicação/Micros/Raspberry_Pi/AES/compile_and_run_Server.sh
+++ Aplicação/Micros/Raspberry_Pi/AES/compile_and_run_Server.sh
@@ -3,16 +3,16 @@ gcc crypto_rasp.c Server.c \
     -I/home/augusto/relic/include \
     -L/home/augusto/relic/relic-target/lib \
     -lrelic_s \
-    -o server
+    -o server.run
 
  for i in {0..50}
 do
- 	./server $i /home/augusto/Documentos/criptografia/Aplicação/Micros/Raspberry_Pi/AES/Imagens/pao_na_chapa.jpg 10.0.0.9
+ 	./server.run $i /home/augusto/Documentos/criptografia/Aplicação/Micros/Raspberry_Pi/AES/Imagens/pao_na_chapa.jpg 10.0.0.9
  	sleep 1s
 done   
 
 for i in {0..50}
 do
- 	./server $i -f /home/augusto/Documentos/criptografia/Aplicação/Micros/Raspberry_Pi/AES/Imagens/pao_na_chapa.jpg 10.0.0.9
+ 	./server.run $i -f /home/augusto/Documentos/criptografia/Aplicação/Micros/Raspberry_Pi/AES/Imagens/pao_na_chapa.jpg 10.0.0.9
  	sleep 1s
 done   
